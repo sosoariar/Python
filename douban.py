aa6fe0558811cb4e736f2acb1e94a8752dccd7be
SoSo <sosoariar@163.com>
2024-02-05 07:15:15 +0800

    project init

--- douban.py
+++ douban.py
@@ -1,19 +1,24 @@
 import requests
-
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-
-header={}
-header['user-agent']=user_agent
-
-url = 'https://book.douban.com/tag/%E6%8E%A8%E7%90%86'
-
-response = requests.get(url,headers=header)
-
 from bs4 import BeautifulSoup as bs
-bs_info = bs(response.text,'html.parser')
-print(bs_info.find_all('div',attrs={'class':'info'})[0])
+from time import sleep
 
-for tags in bs_info.find_all('div',attrs={'class':'info'}):
-    for atag in tags.find_all('a',):
-        print(atag.get('href'))
-        print(atag.get('title'))
+def get_url_name(url_args):
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+
+    header={}
+    header['user-agent']=user_agent
+
+    response = requests.get(url_args,headers=header)
+    bs_info = bs(response.text,'html.parser')
+
+    for tags in bs_info.find_all('div',attrs={'class':'info'}):
+        for atag in tags.find_all('a',):
+            print(atag.get('href'))
+            print(atag.get('title'))
+
+urls = tuple(f'https://book.douban.com/top250?start={page * 25}' for page in range(3))
+
+if __name__ == '__main__':
+    for page in urls:
+        get_url_name(page)
+        sleep(5)
